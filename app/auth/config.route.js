929c2e47abaae344442067c3ce3d708dbba486f0
LiberalOtaku <ledgam3r1279@gmail.com>
2016-06-22 13:26:31 -0400

    AuthController added.

--- app/auth/config.route.js
+++ app/auth/config.route.js
@@ -1,5 +1,17 @@
 (function() {
   'use strict';
 
-  angular.module('mutantApp.auth', []);
+  angular
+    .module('mutantApp.auth')
+    .config(configFunction);
+
+  configFunction.$inject = ['$stateProvider'];
+  function configFunction($stateProvider) {
+    $stateProvider.state('register', {
+      url: '/register',
+      templateUrl: 'app/auth/register.html',
+      controller: 'AuthController',
+      controllerAs: 'vm'
+    });
+  }
 })();
