cff214d7b212e8a94aa5e1cb04b166327eb5b133
LiberalOtaku <ledgam3r1279@gmail.com>
2016-06-24 15:12:14 -0400

    Redirect to login page on auth error.

--- app/app.module.js
+++ app/app.module.js
@@ -16,10 +16,22 @@
       'mutantApp.core',
       'mutantApp.layout'
     ])
-    .config(configFunction);
+    .config(configFunction)
+    .run(runFunction);
 
   configFunction.$inject = ['$urlRouterProvider'];
   function configFunction($urlRouterProvider) {
     $urlRouterProvider.otherwise('/');
+  }
+
+  runFunction.$inject = ['$rootScope', '$state'];
+  function runFunction($rootScope, $state) {
+    $rootScope.$on('$stateChangeError', function(event, toState, toParams, fromState, fromParams, error) {
+      // We can catch the error thrown when the $requireAuth promise is rejected
+      // and redirect the user back to the login page
+      if (error === 'AUTH_REQUIRED') {
+        $state.go('login');
+      }
+    });
   }
 })();
