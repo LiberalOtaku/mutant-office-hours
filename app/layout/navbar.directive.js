3d50ca4d8045299e076dcb23e9c178e8954fce85
LiberalOtaku <ledgam3r1279@gmail.com>
2016-06-24 14:50:44 -0400

    Move logout functionality to navbar directive.

--- app/layout/navbar.directive.js
+++ app/layout/navbar.directive.js
@@ -8,7 +8,22 @@
   function myNavbar() {
     return {
       restrict: 'AE',
-      templateUrl: 'app/layout/navbar.html'
+      templateUrl: 'app/layout/navbar.html',
+      controller: NavbarController,
+      controllerAs: 'vm'
     };
+  }
+
+  NavbarController.$inject = ['$state', 'authService']
+  function NavbarController($state, authService) {
+    var vm = this;
+
+    vm.logout = logout;
+    vm.isLoggedIn = authService.isLoggedIn;
+
+    function logout() {
+      authService.logout();
+      $state.go('home');
+    }
   }
 })();
