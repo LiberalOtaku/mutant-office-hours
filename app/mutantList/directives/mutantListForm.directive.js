dce268d7bc0a3ddfac8107365339acf39aa3e6b0
LiberalOtaku <ledgam3r1279@gmail.com>
2016-06-28 14:47:39 -0400

    Added controller to mutant form.

--- app/mutantList/directives/mutantListForm.directive.js
+++ app/mutantList/directives/mutantListForm.directive.js
@@ -8,7 +8,26 @@
   function myMutantListForm() {
     return {
       restrict: 'AE',
-      templateUrl: 'app/mutantList/directives/mutantListForm.html'
+      templateUrl: 'app/mutantList/directives/mutantListForm.html',
+      controller: MutantFormController,
+      controllerAs: 'vm',
+      bindToController: true,
+      scope: {
+        mutants: '='
+      }
     };
+  }
+
+  MutantFormController.$inject = ['mutantService'];
+  function MutantFormController(mutantService, $scope) {
+    var vm = this;
+
+    vm.addMutant = addMutant;
+    vm.newMutant = new mutantService.Mutant();
+
+    function addMutant() {
+      vm.mutants.$add(vm.newMutant);
+      vm.newMutant = new mutantService.Mutant();
+    }
   }
 })();
